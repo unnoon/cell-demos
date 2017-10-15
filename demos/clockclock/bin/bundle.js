ebe80eb65082f5dc82d958b51c1bd6fe0daacea3
grogger.io <r.geertzema@gmail.com>
2017-10-15 16:40:02 +0200

    update cell-engine 0.1.3

--- demos/clockclock/bin/bundle.js
+++ demos/clockclock/bin/bundle.js
@@ -70376,7 +70376,7 @@ class SceneLoader {
             sceneNode.interactive = true;
             sceneNode.buttonMode = true;
             if (on.click) {
-                sceneNode.on('click', () => this.events.push({ event: on.click, sender: on.eid }));
+                sceneNode.on('pointerdown', () => this.events.push({ event: on.click, sender: on.eid }));
             }
         }
         let children;
